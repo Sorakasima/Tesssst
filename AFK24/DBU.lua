f039778ca139e20e4bade5eef33d4783246f2a14
Sorakasima <khangtocdomax@gmail.com>
2024-06-01 23:29:19 +0700

    Update DBU.lua

--- AFK24/DBU.lua
+++ AFK24/DBU.lua
@@ -1380,7 +1380,7 @@ end
                                                 local myChar = game.Players.LocalPlayer.Character
                                                 
                                                 myChar.HumanoidRootPart.CFrame =
-                                                    Echar:GetPrimaryPartCFrame() * CFrame.new(0, 0, 1.5)
+                                                    Echar:GetPrimaryPartCFrame() * CFrame.new(0, 0, 1.8)
 
                                                 task.wait()
                                             until not autoFarmLoopRunning or v.Humanoid.Health <= 0 or
