9783782940209a009f56b9d0b2aa901de173d4a8
Sorakasima <khangtocdomax@gmail.com>
2024-06-01 20:40:55 +0700

    Update DBU.lua

--- AFK24/DBU.lua
+++ AFK24/DBU.lua
@@ -987,7 +987,7 @@ local function getQuest(switch1)
 end
 questneeded = false
 -- Function
-local attacks2 = {"Super Dragon Fist", "God Slicer", "Spirit Barrage", "Mach Kick", "Wolf Fang Fist", "Meteor Crash", "High Power Rush"}
+local attacks2 = {"Super Dragon Fist", "God Slicer", "Spirit Barrage", "Mach Kick", "Meteor Crash", "Wolf Fang Fist", "High Power Rush"}
 
 -- Function
 local attacks = { "Vital Strike", "Uppercut",
