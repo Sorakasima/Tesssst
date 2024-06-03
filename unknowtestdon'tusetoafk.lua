0fad1e199e561a8c3cd3f29c96c4307addb3fa32
Sorakasima <khangtocdomax@gmail.com>
2024-06-03 09:06:07 +0700

    Update unknowtestdon'tusetoafk.lua

--- unknowtestdon'tusetoafk.lua
+++ unknowtestdon'tusetoafk.lua
@@ -1394,7 +1394,7 @@ end
                                         repeat
                                             task.wait()
                                             
-                                            if checkValue >= 7000 and game.Workspace.Living[targetted].Stats.Ki.Value >
+                                            if checkValue >= 7000 or game.Workspace.Living[targetted].Stats.Ki.Value >
                                                 100 then -- op move stack 1
 
                                                 repeat
