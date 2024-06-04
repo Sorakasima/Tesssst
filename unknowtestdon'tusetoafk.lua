dc0a582305a2dc7b77a7ae830e3e1a5f083a0b68
Sorakasima <khangtocdomax@gmail.com>
2024-06-04 09:27:14 +0700

    Update unknowtestdon'tusetoafk.lua

--- unknowtestdon'tusetoafk.lua
+++ unknowtestdon'tusetoafk.lua
@@ -987,7 +987,7 @@ local function getQuest(switch1)
 end
 questneeded = false
 -- Function
-local attacks2 = {"God Slicer", "Spirit Barrage", 
+local attacks2 = {"Super Dragon Fist", "God Slicer", "Spirit Barrage", 
 "Mach Kick","Meteor Crash", "Wolf Fang Fist", "High Power Rush"}
 
 -- Function
