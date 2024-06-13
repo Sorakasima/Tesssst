f5707ffbc693ade191d81f855f8cdf056b9db458
Sorakasima <khangtocdomax@gmail.com>
2024-06-14 06:36:53 +0700

    Update Tesst.lua

--- Tesst.lua
+++ Tesst.lua
@@ -1,79 +1,79 @@
-return {{
+return { {
     name = "X Fighter Trainer",
     nickname = "X Fighter",
     requiredValue = 0,
-    endRange = 10000,
+    endRange = 30000,
     planet = "Earth"
 }, {
     name = "Klirin",
     nickname = "Klirin",
-    requiredValue = 10000,
-    endRange = 30000 ,
+    requiredValue = 30001,
+    endRange = 60000,
     planet = "Earth"
 }, {
     name = "Kid Nohag",
     nickname = "Kid Nohag",
-    requiredValue = 30001,
-    endRange = 60000 ,
+    requiredValue = 60001,
+    endRange = 80000,
     planet = "Earth"
 }, {
     name = "Radish",
     nickname = "Radish",
-    requiredValue = 60001,
+    requiredValue = 80001,
     endRange = 150000,
     planet = "Earth"
 }, {
     name = "Mapa",
     nickname = "Mapa",
     requiredValue = 150001,
-    endRange = 300000,
+    endRange = 400000,
     planet = "Earth"
 }, {
     name = "Top X Fighter",
     nickname = "X Fighter Master",
-    requiredValue = 300001,
-    endRange = 550000,
+    requiredValue = 400001,
+    endRange = 750000,
     planet = "Earth"
 }, {
     name = "Super Vegetable",
     nickname = "Super Vegetable",
-    requiredValue = 550001,
-    endRange = 1000000,
+    requiredValue = 750001,
+    endRange = 1500000,
     planet = "Earth"
 }, {
     name = "Chilly",
     nickname = "Chilly",
-    requiredValue = 1000001,
-    endRange = 3500000,
+    requiredValue = 1500001,
+    endRange = 2575000,
     planet = "Earth"
 }, {
     name = "Perfect Atom",
     nickname = "Perfect Atom",
-    requiredValue = 3500001,
-    endRange = 4000000,
+    requiredValue = 2575001,
+    endRange = 4100000,
     planet = "Earth"
 }, {
     name = "SSJ2 Wukong",
     nickname = "SSJ2 Wukong",
-    requiredValue = 4000001,
+    requiredValue = 4100001,
     endRange = 5000000,
     planet = "Earth"
 }, {
     name = "SSJB Wukong",
     nickname = "SSJB Wukong",
     requiredValue = 5000001,
-    endRange = 15000000,
+    endRange = 17500000,
     planet = "Earth"
 }, {
     name = "Broccoli",
     nickname = "Broccoli",
-    requiredValue = 15000001,
-    endRange = 50000000,
+    requiredValue = 17500001,
+    endRange = 150000000,
     planet = "Earth"
 }, {
     name = "SSJG Kakata",
     nickname = "SSJG Kakata",
-    requiredValue = 50000001,
+    requiredValue = 50000000,
     endRange = 150000000,
     planet = "Earth"
 }, {
@@ -98,18 +98,18 @@ return {{
     name = "Vis (20%)",
     nickname = "Vis (20%)",
     requiredValue = 500000001,
-    endRange = 800000000,
+    endRange = 700000000,
     planet = "Bills"
 }, {
     name = "Vegetable (LBSSJ4)",
     nickname = "Vegetable (LBSSJ4)",
-    requiredValue = 800000001,
-    endRange = 1250000000,
+    requiredValue = 700000001,
+    endRange = 1200000000,
     planet = "Bills"
 }, {
     name = "Wukong (LBSSJ4)",
     nickname = "Wukong (LBSSJ4)",
-    requiredValue = 1250000001,
+    requiredValue = 1200000001,
     endRange = 1950000000,
     planet = "Bills"
 }, {
@@ -128,6 +128,6 @@ return {{
     name = "Vekuta (SSJBUI)",
     nickname = "Vekuta (SSJBUI)",
     requiredValue = 2700000001,
-    endRange = 900000000000000000000000,
+    endRange = 2000000000000000000,
     planet = "Bills"
-}}
+} }
