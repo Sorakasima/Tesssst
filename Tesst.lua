e231854e835fbc93c86624e3ec42823f164f58fc
Sorakasima <khangtocdomax@gmail.com>
2024-06-14 12:18:19 +0700

    Update Tesst.lua

--- Tesst.lua
+++ Tesst.lua
@@ -110,24 +110,24 @@ return {  {
     name = "Wukong (LBSSJ4)",
     nickname = "Wukong (LBSSJ4)",
     requiredValue = 1200000001,
-    endRange = 1600000000,
+    endRange = 2000000000,
     planet = "Bills"
 }, {
     name = "Vekuta (LBSSJ4)",
     nickname = "Vekuta (LBSSJ4)",
-    requiredValue = 1600000001,
-    endRange = 2800000000,
+    requiredValue = 2000000001,
+    endRange = 3200000000,
     planet = "Bills"
 }, {
     name = "Wukong Rose",
     nickname = "Wukong Rose",
-    requiredValue = 2800000001,
-    endRange = 3300000000,
+    requiredValue = 3200000001,
+    endRange = 5000000000,
     planet = "Bills"
 }, {
     name = "Vekuta (SSJBUI)",
     nickname = "Vekuta (SSJBUI)",
-    requiredValue = 3300000001,
+    requiredValue = 5000000001,
     endRange = 900000000000000000000000,
     planet = "Bills"
 }  }
